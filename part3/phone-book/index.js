e22a3f6ea097aeb894d4f2480e836301383fc65b
Tomas Viñals <tomas.vinals@gmail.com>
2025-04-14 17:04:07 -0300

    Finish excercise 3.5

--- part3/phone-book/index.js
+++ part3/phone-book/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 let persons = [
 	{
 		name: "Arto Hellas",
@@ -55,9 +57,26 @@ app.delete("/api/persons/:id", (request, response) => {
     const id = Number(request.params.id);
     persons = persons.filter(person => person.id !== id)
     response.status(200).end();
-    ;})
+});
+ 
 
-    
+app.post("/api/persons", (request, response) => {
+    const body = request.body;
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: "name or number missing"
+        });
+    }
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: Math.max(...ids) + 1
+    };
+    persons = persons.concat(person);
+    response.json(person);
+});
+
+
 const PORT = 3001;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
