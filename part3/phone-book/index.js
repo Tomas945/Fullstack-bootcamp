2287c0c681a198edf86a73be022f6ff768aaa49e
Tomas Viñals <tomas.vinals@gmail.com>
2025-04-29 18:30:51 -0300

    Finish excersice 3.15-3.16

--- part3/phone-book/index.js
+++ part3/phone-book/index.js
@@ -5,10 +5,10 @@ const cors = require("cors");
 const Phone = require("./models/Phone");
 const app = express();
 
+
 app.use(express.json());
 app.use(morgan('tiny'));
 app.use(cors());
-
 
 
 app.get("/", (request, response) => {
@@ -23,16 +23,24 @@ app.get("/api/persons", (request, response) => {
 });
 
 
-app.get("/api/persons/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response,next) => {
 	Phone.findById(request.params.id).then(person => {
-		response.json(person)
+		if (person) {
+			response.json(person)
+		} else {
+			response.status(404).end()
+		}
 	})
-});
+	.catch(error => {
+		next(error)
+	})
+})
 
 app.delete("/api/persons/:id", (request, response) => {
 	Phone.findByIdAndDelete(request.params.id).then(() => {
 		response.status(204).end()
 	})
+	.catch(error => next(error))
 });
  
 
@@ -48,6 +56,17 @@ app.post("/api/persons", (request, response) => {
 		response.json(person)
 	})
 });
+const errorHandler = (error, request, response, next) => {
+	console.error(error.message)
+  
+	if (error.name === 'CastError') {
+	  return response.status(400).send({ error: 'malformatted id' })
+	} 
+  
+	next(error)
+  }
+
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT ;
